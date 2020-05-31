2c5a1f195a191a43b11356876b89549283661ba5
bernov <bernov@gmail.com>
2020-05-31 15:49:45 +0600

    JS Controllers

---
+++ app.js
@@ -0,0 +1,10 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function () {})();
+
+// UI Controller
+const UICtrl = (function () {})();
+
+// App Controller
+const App = (function (ItemCtrl, UICtrl) {})(ItemCtrl, UICtrl);
